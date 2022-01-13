e8fe842a5d760a701d26f5078198ec37ee7dfe54
Vinicius <viniciusmvn@pm.me>
2022-01-13 16:11:21 -0300

    fix: change account initialization contract

--- src/operations/services/interfaces/IAccountInitializationService.ts
+++ src/operations/services/interfaces/IAccountInitializationService.ts
@@ -1,7 +1,16 @@
 import { Initialize, Operation } from '../../models/operation.model';
 
 export interface IAccountInitializationService {
-  perform(file: Operation<Initialize>[]): Promise<unknown[]>;
-  getInitializedAccounts(): Initialize[];
+  perform(file: Operation<Initialize>[]): Promise<Operation<Initialize>[]>;
+  checkInitializedAccounts({
+    sender_document,
+    receiver_document,
+  }: {
+    sender_document?: string;
+    receiver_document?: string;
+  }): {
+    sender: Initialize;
+    receiver: Initialize;
+  };
   updateAvailableLimits(account: Initialize): void;
 }
