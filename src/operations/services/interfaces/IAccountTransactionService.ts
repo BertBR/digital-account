affb84fccad708ba211339136408e21197bbd8db
Vinicius <viniciusmvn@pm.me>
2022-01-13 16:13:15 -0300

    fix: change account transaction contract

--- src/operations/services/interfaces/IAccountTransactionService.ts
+++ src/operations/services/interfaces/IAccountTransactionService.ts
@@ -1,5 +1,5 @@
 import { Operation, Transaction } from '../../models/operation.model';
 
 export interface IAccountTransactionService {
-  perform(file: Operation<Transaction>[]): Promise<unknown>;
+  perform(file: Operation<Transaction>[]): Promise<Operation<Transaction>[]>;
 }
