04d8a6ccd97a95662561df2a47507f26c65d3773
Vinicius <viniciusmvn@pm.me>
2022-01-13 16:14:21 -0300

    feat: add more validations to input file

--- src/operations/shared/FileValidationHelper.ts
+++ src/operations/shared/FileValidationHelper.ts
@@ -2,21 +2,12 @@ import {
   Initialize,
   Operation,
   OperationType,
+  Transaction,
+  TransactionHistory,
 } from '../models/operation.model';
 
-type FileValidationReturnType = {
-  data: Operation<unknown>[];
-  output: Output[];
-};
-
-type Output = {
-  type: OperationType;
-  status: string;
-  violation: string;
-};
-
 export class FileValidationHelper {
-  async perform(file: Buffer): Promise<unknown> {
+  async perform(file: Buffer): Promise<Operation<unknown>[]> {
     try {
       const data = JSON.parse(file.toString()) as Operation<unknown>[];
       let output: unknown;
@@ -26,7 +17,7 @@ export class FileValidationHelper {
             item: data.indexOf(line),
             type: line.type,
             status: 'failure',
-            violation: 'invalid data',
+            violation: 'invalid operation',
           };
           const idx = data.indexOf(line);
           data[idx] = output as Operation<unknown>;
@@ -34,6 +25,7 @@ export class FileValidationHelper {
         }
 
         if (line.type === OperationType.initialize_account) {
+          this.isValidAccountInitialization(line.payload as Initialize);
           if (!this.validateAccountInitialization(line.payload as Initialize)) {
             output = {
               item: data.indexOf(line),
@@ -43,13 +35,45 @@ export class FileValidationHelper {
             };
             const idx = data.indexOf(line);
             data[idx] = output as Operation<unknown>;
-            continue;
           }
+          continue;
+        }
+
+        if (line.type === OperationType.transaction) {
+          this.isValidTransaction(line.payload as Transaction);
+          continue;
+        }
+        if (line.type === OperationType.transaction_history) {
+          this.isValidTransactionHistory(line.payload as TransactionHistory);
+          continue;
         }
       }
       return data;
     } catch (error) {
       throw new Error('invalid input file');
+    }
+  }
+
+  private isValidAccountInitialization(payload: Initialize): void {
+    if (!payload.available_limit || !payload.document || !payload.name) {
+      throw new Error();
+    }
+  }
+
+  private isValidTransaction(payload: Transaction): void {
+    if (
+      !payload.datetime ||
+      !payload.receiver_document ||
+      !payload.sender_document ||
+      !payload.value
+    ) {
+      throw new Error();
+    }
+  }
+
+  private isValidTransactionHistory(payload: TransactionHistory): void {
+    if (!payload.document) {
+      throw new Error();
     }
   }
 
